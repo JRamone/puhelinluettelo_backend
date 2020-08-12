a76ed4d33c8d0de2adc1f16d2b1a1ff3dafff61b
JRamone <jmkoho@gmail.com>
2020-08-12 10:32:42 +0300

    add front

---
+++ build/precache-manifest.41f971d0b73e99acb6af65bfa6d44e39.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b2871b8c1f365fb58165134abe0aa27",
+    "url": "/index.html"
+  },
+  {
+    "revision": "126860b562c0948aba16",
+    "url": "/static/css/main.050523cc.chunk.css"
+  },
+  {
+    "revision": "bade6badc4ee71057448",
+    "url": "/static/js/2.483051de.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.483051de.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "126860b562c0948aba16",
+    "url": "/static/js/main.c6bf56b4.chunk.js"
+  },
+  {
+    "revision": "f0be2d6b237b3fb64219",
+    "url": "/static/js/runtime-main.24b8e7b7.js"
+  }
+]);
--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express()
 const morgan = require('morgan')
 const cors = require('cors')
 app.use(express.json())
+app.use(express.static('build'))
 
 morgan.token('body', function (req, res) { 
     return JSON.stringify(req.body)
